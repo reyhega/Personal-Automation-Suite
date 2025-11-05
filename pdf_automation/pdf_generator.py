b13cb2a3352899b1ff8b2c300f4178b7baa9e1f2
reyhega <reyhega@gmail.com>
2025-11-05 01:48:59 -0600

    Filename, title and body can now be set from the CLI instead of being hardcoded

--- pdf_automation/pdf_generator.py
+++ pdf_automation/pdf_generator.py
@@ -1,4 +1,6 @@
 import argparse
+from functools import partial
+
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.rl_config import defaultPageSize
@@ -6,26 +8,23 @@ from reportlab.lib.units import inch
 PAGE_HEIGHT = defaultPageSize[1]; PAGE_WIDTH = defaultPageSize[0]
 styles = getSampleStyleSheet()
 
-title = "Hello world"
-body = "This is the body"
 author = "Rey HeGa"
-filename = "Generated.pdf"
 
-def myFirstPage(canvas, doc):
+def first_page_header(canvas, doc, custom_title):
     canvas.saveState()
     canvas.setFont('Helvetica-Bold', 16)
-    canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-108, title)
+    canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-108, custom_title)
     canvas.setFont('Helvetica', 9)
-    canvas.drawString(inch, 0.75 * inch, "Page %d %s" % (doc.page, author))
+    canvas.drawString(PAGE_WIDTH-90, 0.75 * inch, "Page %d " % doc.page)
     canvas.restoreState()
 
-def myLaterPages(canvas, doc):
+def later_pages_header(canvas, doc):
     canvas.saveState()
     canvas.setFont('Helvetica', 9)
-    canvas.drawString(inch, 0.75 * inch, "Page %d %s" % (doc.page, author))
+    canvas.drawString(PAGE_WIDTH-90, 0.75 * inch, "Page %d " % doc.page)
     canvas.restoreState()
 
-def go(filename, body):
+def generate_basic_report(filename, title, body):
     doc = SimpleDocTemplate(filename)
     Story = [Spacer(1, 2*inch)]
     style = styles["Normal"]
@@ -34,9 +33,21 @@ def go(filename, body):
     Story.append(p)
     Story.append(Spacer(1, 0.2*inch))
 
-    doc.build(Story, onFirstPage=myFirstPage, onLaterPages=myLaterPages)
+    first_page_func = partial(first_page_header, custom_title=title)
+
+    doc.build(Story, onFirstPage=first_page_func, onLaterPages=later_pages_header)
 
 
 if __name__=="__main__":
 
-    go(filename, body)
+    """title = "Hello world"
+    body = "This is the body"
+    filename = "Generated.pdf"""
+
+    parser = argparse.ArgumentParser(description='Generate PDF file')
+    parser.add_argument('--filename', required=True, help='Name with which the file will be created')
+    parser.add_argument('--title', default="", help='Title of document')
+    parser.add_argument('--body', default="", help='Body text of document')
+    args = parser.parse_args()
+
+    generate_basic_report(args.filename, args.title, args.body)
