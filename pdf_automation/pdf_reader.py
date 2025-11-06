fef2411d03a654e4f43aaf03e7ac298a75a4952b
reyhega <reyhega@gmail.com>
2025-11-06 00:02:59 -0600

    pdf_reader writes extracted text to a txt file, if file already exists, it adds a timestamp to it

--- pdf_automation/pdf_reader.py
+++ pdf_automation/pdf_reader.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from datetime import datetime
 from PyPDF2 import PdfReader
 
 def extract_text_from_pdf(filepath):
@@ -14,6 +15,19 @@ def extract_text_from_pdf(filepath):
     reader = PdfReader(filepath)
     page = reader.pages[0]
     extracted_text = page.extract_text()
+    extracted_txt_file = "resources/Extracted_text.txt"
+    timestamp = datetime.now().strftime("%H:%M:%S|%m%d%Y")
+
+    if os.path.exists(extracted_txt_file):
+        extracted_txt_file = f"resources/Extracted_text_{timestamp}.txt"
+    
+    try:
+        with open(extracted_txt_file, "w") as file:
+            file.write(extracted_text)
+            print(f"✅ Extracted text from {filepath} → {extracted_txt_file}")
+    except Exception as e:
+            print(f"❌ Failed to extract text from {filepath}: {e}")
+
     print(extracted_text)
     return extracted_text
 
