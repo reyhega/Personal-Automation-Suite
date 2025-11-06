7d33336d37be6493e8611eabc1371b6de1edb80b
reyhega <reyhega@gmail.com>
2025-11-06 02:57:37 -0600

    User can now select  between reading or modifying metadata, if modifying, they are asked to input metadata changes

--- pdf_automation/pdf_metadata.py
+++ pdf_automation/pdf_metadata.py
@@ -4,33 +4,32 @@ from datetime import datetime
 from PyPDF2 import PdfReader, PdfWriter
 
 
-"""def read_metadata(filepath):
+def read_metadata(filepath):
     reader = PdfReader(filepath)
-
     meta = reader.metadata
     num_pages = len(reader.pages)
 
     print(f"Number of pages: {num_pages}")
-
     # All of the following could be None:
     print(f"Author: {meta.author}")
     print(f"Creator: {meta.creator}")
     print(f"Producer: {meta.producer}")
     print(f"Subject: {meta.subject}")
     print(f"Title: {meta.title}")
-"""
 
-def add_metadata(filepath):
+
+def add_metadata(filepath, author, creator, producer, subject, title):
     reader = PdfReader(filepath)
     writer = PdfWriter()
     meta = reader.metadata
+    num_pages = len(reader.pages)
     old_meta = {
-        "num_pages": len(reader.pages),
-        "author": meta.author,
-        "creator": meta.creator,
-        "producer": meta.producer,
-        "subject": meta.subject,
-        "title": meta.title,
+        # "num_pages": len(reader.pages),
+        "Author": meta.author,
+        "Creator": meta.creator,
+        "Producer": meta.producer,
+        "Subject": meta.subject,
+        "Title": meta.title,
     }
     
     # Add pages to the writer
@@ -40,8 +39,11 @@ def add_metadata(filepath):
     # Add the metadata
     writer.add_metadata(
         {
-            "/Author": "Rey HeGa",
-            "/Creator": "Rey Hdz",
+            "/Author": author,
+            "/Creator": creator,
+            "/Producer": producer,
+            "/Subject": subject,
+            "/Title": title,
         }
     )
 
@@ -53,29 +55,47 @@ def add_metadata(filepath):
     new_reader = PdfReader(new_file)
     new_meta = new_reader.metadata
     new_file_meta = {
-        "num_pages": len(new_reader.pages),
-        "author": new_meta.author,
-        "creator": new_meta.creator,
-        "producer": new_meta.producer,
-        "subject": new_meta.subject,
-        "title": new_meta.title,
+        # "num_pages": len(new_reader.pages),
+        "Author": new_meta.author,
+        "Creator": new_meta.creator,
+        "Producer": new_meta.producer,
+        "Subject": new_meta.subject,
+        "Title": new_meta.title,
     }
 
     print(f"✅ Succesfully created copy of file with following metadata:")
+    print("---------------------------------------------------------------------------------------")
+    print(f"Number of pages: {num_pages}")
     for key in old_meta:
         old_value = old_meta[key]
         new_value = new_file_meta[key]
-        print(f"☑️ Changed {key} from '{old_value}' → '{new_value}'")
+        print(f"☑️ Set '{key}' from:  '{old_value}'   →   '{new_value}'")
+    print("---------------------------------------------------------------------------------------")
+    print(f"📁 File saved at location: '{new_file}'")
 
-
-"""if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Extract text from PDF file')
-    parser.add_argument('--filepath', required=True, help='Path to PDF file to extract text from')
-    args = parser.parse_args()
-    extract_text_from_pdf(args.filepath)"""
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read and modify PDF metadata')
     parser.add_argument('--filepath', required=True, help='Path to PDF file')
+    parser.add_argument('--read', action='store_const', const=read_metadata, help='Read metadata of PDF file')
+    parser.add_argument('--add', action='store_const', const=add_metadata, help='Modify metadata of PDF file')
     args = parser.parse_args()
-    add_metadata(args.filepath)
+
+    if args.read:
+        print("=======================================================================================")
+        print(f"🔎 Here's the file's metadata:")
+        print("---------------------------------------------------------------------------------------")
+        read_metadata(args.filepath)
+        print("---------------------------------------------------------------------------------------")
+    elif args.add:
+        print("=======================================================================================")
+        print("📝 Please provide the following info (can be 'None')")
+        print("---------------------------------------------------------------------------------------")
+        i_author = input("Author: ")
+        i_creator = input("Creator: ")
+        i_producer = input("Producer: ")
+        i_subject = input("Subject: ")
+        i_title = input("Title: ")
+        print("---------------------------------------------------------------------------------------")
+        add_metadata(args.filepath, i_author, i_creator, i_producer, i_subject, i_title)
+        print("---------------------------------------------------------------------------------------")
